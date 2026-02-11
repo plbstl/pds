9a465b9a1a0d407708bbb0b79c7cab8dd578737d
Paul Ebose <pebose@proton.me>
2026-02-11 20:11:23 +0100

    simplify module and tests setup in build.zig

--- build.zig
+++ build.zig
@@ -1,52 +1,15 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
-
-    const mod = b.addModule("pds", .{
+    const pds_mod = b.addModule("pds", .{
         .root_source_file = b.path("src/root.zig"),
-        .target = target,
+        .target = b.standardTargetOptions(.{}),
+        .optimize = b.standardOptimizeOption(.{}),
     });
 
-    const exe = b.addExecutable(.{
-        .name = "pds",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = optimize,
-            .imports = &.{
-                .{ .name = "pds", .module = mod },
-            },
-        }),
-    });
-
-    b.installArtifact(exe);
-
-    const run_step = b.step("run", "Run the app");
-
-    const run_cmd = b.addRunArtifact(exe);
-    run_step.dependOn(&run_cmd.step);
-
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
-    const mod_tests = b.addTest(.{
-        .root_module = mod,
-    });
-
-    const run_mod_tests = b.addRunArtifact(mod_tests);
-
-    const exe_tests = b.addTest(.{
-        .root_module = exe.root_module,
-    });
-
-    const run_exe_tests = b.addRunArtifact(exe_tests);
-
-    const test_step = b.step("test", "Run tests");
-    test_step.dependOn(&run_mod_tests.step);
-    test_step.dependOn(&run_exe_tests.step);
+    // zig build test
+    const test_step = b.step("test", "Run unit tests");
+    const pds_mod_tests = b.addTest(.{ .root_module = pds_mod });
+    const run_pds_mod_tests = b.addRunArtifact(pds_mod_tests);
+    test_step.dependOn(&run_pds_mod_tests.step);
 }
