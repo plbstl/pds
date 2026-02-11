a0220e90233cbc4c822eb7758aae6d52f6ebf1f0
Paul Ebose <pebose@proton.me>
2026-02-11 21:36:02 +0100

    expose BloomFilter and include tests for all referenced symbols in root.zig

--- src/root.zig
+++ src/root.zig
@@ -1,18 +1,10 @@
-//! By convention, root.zig is the root source file when making a package.
-const std = @import("std");
-const Io = std.Io;
+//! Probabilistic data structures for approximate queries on large or
+//! streaming data. These structures trade exactness for speed and
+//! memory efficiency.
+//!
 
-/// This is a documentation comment to explain the `printAnotherMessage` function below.
-///
-/// Accepting an `Io.Writer` instance is a handy way to write reusable code.
-pub fn printAnotherMessage(writer: *Io.Writer) Io.Writer.Error!void {
-    try writer.print("Run `zig build test` to run the tests.\n", .{});
-}
+pub const BloomFilter = @import("BloomFilter.zig");
 
-pub fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
-
-test "basic add functionality" {
-    try std.testing.expect(add(3, 7) == 10);
+test {
+    @import("std").testing.refAllDecls(@This());
 }
